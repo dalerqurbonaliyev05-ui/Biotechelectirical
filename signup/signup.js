11cb075f1862888f711d0f3b31140c6ad241bfac
dalerqurbonaliyev05-ui <dalerqurbonaliyev05@gmail.com>
2026-03-15 20:21:19 +0500

    Update signup.js

--- signup/signup.js
+++ signup/signup.js
@@ -1,3 +1,19 @@
+import { GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
+const provider = new GoogleAuthProvider();
+
+// Google tugmasini bosganda
+const googleBtn = document.querySelector('.fa-google-plus-g').parentElement; 
+googleBtn.addEventListener('click', () => {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            alert("Google orqali kirdingiz!");
+            window.location.href = "signup.html";
+        }).catch((error) => {
+            console.error(error.message);
+        });
+});
+
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
